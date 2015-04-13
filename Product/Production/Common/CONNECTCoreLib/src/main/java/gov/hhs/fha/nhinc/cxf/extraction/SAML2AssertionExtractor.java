2339adce6209760b6dda593b76e4180bea004745
Logan Raynes <logan.raynes@orionhealth.com>
2015-04-13 15:52:47 +1200

    The SAML Assertion will now only be retrieved from a Security Header if that Security Header is a SAML Security Header. This change was needed because the OrionHealth Patch adds uses a Digest Security Header.

--- Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/cxf/extraction/SAML2AssertionExtractor.java
+++ Product/Production/Common/CONNECTCoreLib/src/main/java/gov/hhs/fha/nhinc/cxf/extraction/SAML2AssertionExtractor.java
@@ -3,6 +3,7 @@
  */
 package gov.hhs.fha.nhinc.cxf.extraction;
 
+import gov.hhs.fha.nhinc.callback.SamlConstants;
 import gov.hhs.fha.nhinc.common.nhinccommon.AssertionType;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import org.apache.cxf.binding.soap.SoapHeader;
 import org.apache.cxf.headers.Header;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * This class is used to extract the AssertionType from Saml2Assertion in the ws
@@ -59,7 +61,7 @@ public class SAML2AssertionExtractor {
         }
 
         MessageContext mContext = (MessageContext) context.getMessageContext();
-        SoapHeader header = getSecuritySoapHeader(mContext);
+        SoapHeader header = getSamlSecuritySoapHeader(mContext);
 
         if (header != null) {
             Object obj = header.getObject();
@@ -71,20 +73,35 @@ public class SAML2AssertionExtractor {
         return target;
     }
 
-    private final SoapHeader getSecuritySoapHeader(MessageContext mContext) {
+	private final SoapHeader getSamlSecuritySoapHeader(final MessageContext mContext) {
+		Header header = null;
+		if (mContext != null) {
+			@SuppressWarnings("unchecked")
+			final List<Header> headers = (List<Header>) mContext.get(org.apache.cxf.headers.Header.HEADER_LIST);
+			if (headers != null) {
+				for (final Header h : headers) {
+					if (h.getName().getLocalPart().equalsIgnoreCase("Security")) {
+						header = h;
+						break;
+					}
+				}
+			}
+		}
+		// verify that the security header is in fact a SAML security header and not for digest authentication.
+		// This is required for the Orion security patch that uses digest authentication to work.
+		if (null != header && elementContainsSaml2Assertion((Element) header.getObject())) {
+			return (SoapHeader) header;
+		}
+		return null;
+	}
 
-        if (mContext != null) {
-            @SuppressWarnings("unchecked")
-            List<Header> headers = (List<Header>) mContext.get(org.apache.cxf.headers.Header.HEADER_LIST);
-
-            if (headers != null) {
-                for (Header header : headers) {
-                    if (header.getName().getLocalPart().equalsIgnoreCase("Security")) {
-                        return (SoapHeader) header;
-                    }
-                }
-            }
-        }
-        return null;
-    }
+	private boolean elementContainsSaml2Assertion(final Element element) {
+		if (element.getNamespaceURI().equals(SamlConstants.SAML2_ASSERTION_NS)
+				&& element.getLocalName().equals(SamlConstants.SAML2_ASSERTION_TAG)) {
+			return true;
+		}
+		final NodeList assertionNodes = element.getElementsByTagNameNS(SamlConstants.SAML2_ASSERTION_NS,
+				SamlConstants.SAML2_ASSERTION_TAG);
+		return assertionNodes.getLength() > 0;
+	}
 }
